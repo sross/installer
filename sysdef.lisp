73bb5870e4e71df5742d2532e008729747970ec6
Sean Ross <sross@hacktar.local>
2008-10-22 21:48:12 +0100

    bump version

--- sysdef.lisp
+++ sysdef.lisp
@@ -1,7 +1,7 @@
 (in-package :sysdef-user)
 
 (define-system :installer (serial-system)
-  (:version 0 1 2)
+  (:version 0 1 3)
   (:components "package" "installer")
   (:needs :drakma (:mb.sysdef :version (>= 0 1 9)) :puri :cl-ppcre :md5 :gzip-stream :archive))
 
